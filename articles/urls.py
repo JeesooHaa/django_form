788f0b39513c33a08fcfdb0799bb63f58be784a3
JeesooHaa <haajeesoo@gmail.com>
2019-09-26 11:34:27 +0900

    Form

--- articles/urls.py
+++ articles/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
+app_name = 'articles'
+
+# /articles/__
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:article_pk>/', views.detail, name='detail'),
 ]
